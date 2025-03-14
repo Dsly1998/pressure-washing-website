cfb10b58c6523ab8307667eb5c89deb48015e16c
Dallin Sly <krave83@gmail.com>
2025-03-14 18:05:42 -0400

    photos added to gallery

--- src/data/galleryData.js
+++ src/data/galleryData.js
@@ -16,148 +16,497 @@ import afterDriveway from "../images/afterDriveway1224.png";
 import beforeSidewalk from "../images/beforeSidewalk1224.png";
 import afterSidewalk from "../images/afterSidewalk1224.png";
 
+// Add new image imports
+import img8296 from "../images/new/IMG_8296.jpg";
+import img8299 from "../images/new/IMG_8299.jpg";
+import img8363 from "../images/new/IMG_8363.jpg";
+import img8364 from "../images/new/IMG_8364.jpg";
+import img8365 from "../images/new/IMG_8365.jpg";
+import img8366 from "../images/new/IMG_8366.jpg";
+import img8367 from "../images/new/IMG_8367.jpg";
+import img8368 from "../images/new/IMG_8368.jpg";
+import img8370 from "../images/new/IMG_8370.jpg";
+import img8372 from "../images/new/IMG_8372.jpg";
+import img8375 from "../images/new/IMG_8375.jpg";
+import img8378 from "../images/new/IMG_8378.jpg";
+import img8380 from "../images/new/IMG_8380.jpg";
+import img8381 from "../images/new/IMG_8381.jpg";
+import img8402 from "../images/new/IMG_8402.jpg";
+import img8403 from "../images/new/IMG_8403.jpg";
+import img8414 from "../images/new/IMG_8414.jpg";
+import img8415 from "../images/new/IMG_8415.jpg";
+import img8416 from "../images/new/IMG_8416.jpg";
+import img8418 from "../images/new/IMG_8418.jpg";
+import img8592 from "../images/new/IMG_8592.jpg";
+import img8598 from "../images/new/IMG_8598.jpg";
+import img8599 from "../images/new/IMG_8599.jpg";
+import img8604 from "../images/new/IMG_8604.jpg";
+import img8605 from "../images/new/IMG_8605.jpg";
+import img8606 from "../images/new/IMG_8606.jpg";
+import img8607 from "../images/new/IMG_8607.jpg";
+import img8609 from "../images/new/IMG_8609.jpg";
+import img8610 from "../images/new/IMG_8610.jpg";
+import img8612 from "../images/new/IMG_8612.jpg";
+import img8613 from "../images/new/IMG_8613.jpg";
+import img8636 from "../images/new/IMG_8636.jpg";
+import img8639 from "../images/new/IMG_8639.jpg";
+import img8640 from "../images/new/IMG_8640.jpg";
+import img8645 from "../images/new/IMG_8645.jpg";
+import img8647 from "../images/new/IMG_8647.jpg";
+import img8648 from "../images/new/IMG_8648.jpg";
+import img8654 from "../images/new/IMG_8654.jpg";
+import img8658 from "../images/new/IMG_8658.jpg";
+import img8659 from "../images/new/IMG_8659.jpg";
+import img8694 from "../images/new/IMG_8694.jpg";
+import img8696 from "../images/new/IMG_8696.jpg";
+import img8743 from "../images/new/IMG_8743.PNG";
+
 export const galleryData = [
   {
-    id: 'logo',
+    id: "logo",
     image: gallery32,
     alt: "Red Line & Blue Logo",
     caption: "Red Line & Blue Pressure Washing",
-    type: 'single'
+    type: "single",
   },
   {
-    id: 'driveway',
+    id: "driveway",
     before: {
       image: beforeDriveway,
       alt: "Driveway Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: afterDriveway,
       alt: "Driveway After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'sidewalk',
+    id: "sidewalk",
     before: {
       image: beforeSidewalk,
       alt: "Sidewalk Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: afterSidewalk,
       alt: "Sidewalk After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'gallery3',
+    id: "gallery3",
     before: {
       image: gallery33,
       alt: "Surface Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: gallery31,
       alt: "Surface After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'gallery2',
+    id: "gallery2",
     before: {
       image: gallery22,
       alt: "Area Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: gallery21,
       alt: "Area After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'gallery1',
+    id: "gallery1",
     before: {
       image: gallery11,
       alt: "Surface Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: gallery12,
       alt: "Surface After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'before41',
+    id: "before41",
     before: {
       image: before41,
       alt: "Area Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: after41,
       alt: "Area After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'before51',
+    id: "before51",
     before: {
       image: before51,
       alt: "Surface Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: after51,
       alt: "Surface After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
+    type: "comparison",
   },
   {
-    id: 'before61',
+    id: "before61",
     before: {
       image: before61,
       alt: "Area Before Cleaning",
-      caption: "Before"
+      caption: "Before",
     },
     after: {
       image: after61,
       alt: "Area After Cleaning",
-      caption: "After"
+      caption: "After",
     },
-    type: 'comparison'
-  }
+    type: "comparison",
+  },
+  // Add all new single images
+  {
+    id: "img8296",
+    image: img8296,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8299",
+    image: img8299,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8363",
+    image: img8363,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8364",
+    image: img8364,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8365",
+    image: img8365,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8366",
+    image: img8366,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8367",
+    image: img8367,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8368",
+    image: img8368,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8370",
+    image: img8370,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8372",
+    image: img8372,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8375",
+    image: img8375,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8378",
+    image: img8378,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8380",
+    image: img8380,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8381",
+    image: img8381,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8402",
+    image: img8402,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8403",
+    image: img8403,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8414",
+    image: img8414,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8415",
+    image: img8415,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8416",
+    image: img8416,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8418",
+    image: img8418,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8592",
+    image: img8592,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8598",
+    image: img8598,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8599",
+    image: img8599,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8604",
+    image: img8604,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8605",
+    image: img8605,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8606",
+    image: img8606,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8607",
+    image: img8607,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8609",
+    image: img8609,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8610",
+    image: img8610,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8612",
+    image: img8612,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8613",
+    image: img8613,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8636",
+    image: img8636,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8639",
+    image: img8639,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8640",
+    image: img8640,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8645",
+    image: img8645,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8647",
+    image: img8647,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8648",
+    image: img8648,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8654",
+    image: img8654,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8658",
+    image: img8658,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8659",
+    image: img8659,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8694",
+    image: img8694,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8696",
+    image: img8696,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
+  {
+    id: "img8743",
+    image: img8743,
+    alt: "Pressure Washing Service",
+    caption: "Red Line and Blue Pressure Washing",
+    type: "single",
+  },
 ];
 
 export const getGallerySlides = () => {
-  return galleryData.map(item => {
-    if (item.type === 'single') {
-      return [{
-        image: item.image,
-        alt: item.alt,
-        caption: item.caption
-      }];
+  return galleryData.map((item) => {
+    if (item.type === "single") {
+      return [
+        {
+          image: item.image,
+          alt: item.alt,
+          caption: item.caption,
+        },
+      ];
     }
     return [
       {
         image: item.before.image,
         alt: item.before.alt,
-        caption: item.before.caption
+        caption: item.before.caption,
       },
       {
         image: item.after.image,
         alt: item.after.alt,
-        caption: item.after.caption
-      }
+        caption: item.after.caption,
+      },
     ];
   });
-}; 
+};
